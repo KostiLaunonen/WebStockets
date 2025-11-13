6b3d7ec5ff8e9c9ccb13d33d50fec1201cd0aa98
KostiLaunonen <kosti.launonen70@gmail.com>
2025-11-13 21:27:52 +0200

    Backend Refactoring

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT;
 
@@ -8,7 +9,7 @@ const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 
-const latestPrices = {};
+const subscriptions = new Set(['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'BINANCE:BTCUSDT']);
 
 // .. like this
 const server = http.createServer(app);
@@ -16,37 +17,48 @@ const socketServer = new WebSocket.Server({ server });
 
 const socket = new WebSocket(`wss://ws.finnhub.io?token=${API_KEY}`);
 
-// Send a websocket subscription request to Finnhub
-// In this case it's the stock price of Apple and Bitcoin
+// Send a websocket subscription request to Finnhub based on the defined symbols in 'subscriptions' set
 socket.on('open', () => {
-    socket.send(JSON.stringify({ type: 'subscribe', symbol: 'AAPL' }));
-    socket.send(JSON.stringify({ type: 'subscribe', symbol: 'BINANCE:BTCUSDT' }));
+    subscriptions.forEach(symbol => {
+        socket.send(JSON.stringify({ type: 'subscribe', symbol }))
+    })
 });
 
 // A listener for messages from Finnhub
 socket.on('message', (data) => {
     const parsed = JSON.parse(data.toString());
-    const trade = parsed.data[0];
-    latestPrices[trade.s] = trade.p;
-    console.log(`Symboli: ${trade.s}, Hinta:, ${trade.p}`);
+    if (parsed.type === 'trade' && parsed.data && parsed.data.length > 0) {
+        const trade = parsed.data[0];
+        console.log(`Symboli: ${trade.s}, Hinta:, ${trade.p}`);
 
-    socketServer.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ symbol: trade.s, price: trade.p }));
-        }
-    });
-});
-
-// REST get
-app.get('/price/:symbol', (req, res) => {
-    const symbol = req.params.symbol;
-    const price = latestPrices[symbol];
-    if (price) {
-        res.json({ symbol, price });
-    } else {
-        res.status(404).json({ error: 'Symbol or price not found'});
+        socketServer.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ symbol: trade.s, price: trade.p }));
+            }
+        });
     }
 });
+
+// REST
+// Convert the subscriptions set to an array for frontend
+app.get('/subscriptions', (req, res) => {
+    res.json([...subscriptions]);
+})
+
+// Fetches the quote from Finnhub, which include stock data like prices etc.
+app.get('/quote', async (req, res) => {
+    const symbol = req.query.symbol;
+
+    const response = await axios.get('https://finnhub.io/api/v1/quote', {
+        params: {
+            symbol,
+            token: API_KEY
+        }
+    });
+
+    //console.log(response.data);
+    res.json(response.data);
+})
 
 server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
